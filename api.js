9f0539ca9968ca309562bb171bdfae0f2d8ec61e
Adrian Biedrzycki <a@modules.io>
2017-11-17 17:28:14 -0800

    Use minimal three.js build for api.js

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const {ipcRenderer} = require('electron');
-const THREE = require('three-zeo');
+const THREE = require('./lib/three-min.js');
 const jimp = require('jimp');
 const webgl = require('node-webgl2');
 const openvr = require('node-openvr');
